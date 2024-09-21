49f81eef2b08db543074d78a361ce3b369d742a1
Ryota <ryoenoki2013@gmail.com>
2024-09-21 19:03:12 +0900

    update: appbar in bottom navigation

--- lib/view/screens/bottom_navigation.dart
+++ lib/view/screens/bottom_navigation.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:my_ikimono_zukan/view/components/custom_appbar.dart';
 import 'package:my_ikimono_zukan/view/screens/account_screen.dart';
 import 'package:my_ikimono_zukan/view/screens/capture_screen.dart';
 import 'package:my_ikimono_zukan/view/screens/home_screen.dart';
+import 'package:my_ikimono_zukan/view/theme/theme_mode_provider.dart';
 
-class BottomNavigation extends StatefulWidget {
+class BottomNavigation extends ConsumerStatefulWidget {
   const BottomNavigation({super.key});
 
   @override
-  State<BottomNavigation> createState() => _BottomNavigationState();
+  ConsumerState<BottomNavigation> createState() => _BottomNavigationState();
 }
 
-class _BottomNavigationState extends State<BottomNavigation> {
+class _BottomNavigationState extends ConsumerState<BottomNavigation> {
   // 各画面のリスト
   static const _screens = [
     HomeScreen(),
@@ -22,21 +25,24 @@ class _BottomNavigationState extends State<BottomNavigation> {
 
   @override
   Widget build(BuildContext context) {
+    final themeModeState = ref.watch(themeModeNotifierProvider);
     return Scaffold(
+      appBar: CustomAppBar(
+        themeModeProvider: themeModeState,
+        context: context,
+        toggle: (_) {
+          ref.read(themeModeNotifierProvider.notifier).toggle();
+        },
+      ),
       body: _screens[_selectedIndex],
-      // 本題のNavigationBar
       bottomNavigationBar: NavigationBar(
-        // タップされたタブのインデックスを設定（タブ毎に画面の切り替えをする）
         onDestinationSelected: (int index) {
           setState(() {
             _selectedIndex = index;
           });
         },
-        // 選択されているタブの色（公式サイトのまま黄色）
         indicatorColor: const Color(0xffc9f082),
-        // 選択されたタブの設定（設定しないと画面は切り替わってもタブの色は変わらないです）
         selectedIndex: _selectedIndex,
-        // タブ自体の設定（必須項目のため、書かないとエラーになります）
         destinations: const <Widget>[
           NavigationDestination(
             selectedIcon: Icon(Icons.home),
@@ -48,7 +54,8 @@ class _BottomNavigationState extends State<BottomNavigation> {
             label: 'Capture',
           ),
           NavigationDestination(
-            icon: Icon(Icons.person),
+            selectedIcon: Icon(Icons.person),
+            icon: Icon(Icons.person_outline),
             label: 'Profile',
           ),
         ],
--- lib/view/screens/home_screen.dart
+++ lib/view/screens/home_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:my_ikimono_zukan/view/components/custom_appbar.dart';
 import 'package:my_ikimono_zukan/view/components/custom_search_bar.dart';
-import 'package:my_ikimono_zukan/view/theme/theme_mode_provider.dart';
 
 class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
@@ -14,15 +12,7 @@ class HomeScreen extends ConsumerStatefulWidget {
 class _HomeScreenState extends ConsumerState<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    final themeModeState = ref.watch(themeModeNotifierProvider);
-    return Scaffold(
-      appBar: CustomAppBar(
-        themeModeProvider: themeModeState,
-        context: context,
-        toggle: (_) {
-          ref.read(themeModeNotifierProvider.notifier).toggle();
-        },
-      ),
+    return const Scaffold(
       body: const Column(
         children: [
           CustomSearchBar(),
