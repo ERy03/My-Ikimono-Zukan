1147be1206f8b56cb719d13e7546948fd970330f
Ryota <ryoenoki2013@gmail.com>
2024-09-21 17:05:25 +0900

    add: easy localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,31 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:my_ikimono_zukan/env/env.dart';
-import 'package:my_ikimono_zukan/view/screens/account_screen.dart';
+import 'package:my_ikimono_zukan/view/screens/home_screen.dart';
 import 'package:my_ikimono_zukan/view/screens/login_screen.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
   await Supabase.initialize(
     url: Env.supaurl,
     anonKey: Env.key1,
   );
-  runApp(const MyApp());
+  runApp(
+    ProviderScope(
+      child: EasyLocalization(
+        supportedLocales: const [
+          Locale('en'),
+          Locale('ja'),
+        ],
+        path: 'assets/translations',
+        fallbackLocale: const Locale('en'),
+        child: const MyApp(),
+      ),
+    ),
+  );
 }
 
 final supabase = Supabase.instance.client;
@@ -21,6 +37,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Supabase Flutter',
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       theme: ThemeData.dark().copyWith(
         primaryColor: Colors.green,
         textButtonTheme: TextButtonThemeData(
@@ -37,7 +56,7 @@ class MyApp extends StatelessWidget {
       ),
       home: supabase.auth.currentSession == null
           ? const LoginScreen()
-          : const AccountScreen(),
+          : const HomeScreen(),
     );
   }
 }
